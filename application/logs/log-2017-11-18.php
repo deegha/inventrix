a30b97200860af973a0c31ef4f4caba55a0c4a63
Deegha Galkissa <00T884@ual.lk>
2017-11-22 00:18:58 +0530

    initial code push

---
+++ application/logs/log-2017-11-18.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-11-18 04:59:59 --> Unable to load the requested class: Session
+ERROR - 2017-11-18 05:00:05 --> Unable to load the requested class: Session
+ERROR - 2017-11-18 05:01:36 --> Unable to load the requested class: Session
